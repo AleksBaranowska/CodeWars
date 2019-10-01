b76c1fc5764276bdd137b019e9fab4d163983840
Aleksandra Baranowska <olaska@gmail.com>
2019-10-01 12:40:49 +0100

    Initial commit

---
+++ Even or Odd/src/com/ola/EvenOrOdd.java
@@ -0,0 +1,19 @@
+package com.ola;
+
+/**
+ * Create a function (or write a script in Shell) that takes an integer as an
+ * argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
+
+public class EvenOrOdd {
+	public static String even_or_odd(int number) {
+		String answer;
+		if (number % 2 == 0) {
+			return answer = "Even";
+		} else {
+			answer = "Odd";
+		}
+		return answer;
+	}
+
+}
