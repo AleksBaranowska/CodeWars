6d95cfcc25f331229c710311d7de2ff219490e30
Aleksandra Baranowska <olaska@gmail.com>
2019-10-01 22:04:09 +0100

    Initial commit

---
+++ You Can't Code Under Pressure #1/src/com/ola/Java.java
@@ -0,0 +1,13 @@
+package com.ola;
+
+/**
+ * Code as fast as you can! You need to double the integer and return it.
+ */
+
+public class Java {
+	public static int doubleInteger(int i) {
+		// Double the integer and return it!
+		return i * 2;
+	}
+
+}
