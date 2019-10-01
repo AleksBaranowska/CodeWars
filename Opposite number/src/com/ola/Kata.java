a5b6cf94099b06d6514491710424c6280946f4b0
Aleksandra Baranowska <olaska@gmail.com>
2019-10-01 13:55:09 +0100

    Initial commit

---
+++ Opposite number/src/com/ola/Kata.java
@@ -0,0 +1,16 @@
+package com.ola;
+
+/**
+ * Very simple, given a number, find its opposite.
+ * Examples:
+ * 1: -1 
+ * 14: -14 
+ * -34: 34
+ */
+
+public class Kata {
+	public static int opposite(int number) {
+		number = -number;
+		return number;
+	}
+}
