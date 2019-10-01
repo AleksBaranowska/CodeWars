a4b2b0670a70bec8513b628e2df1df2d6dd4d7b7
Aleksandra Baranowska <olaska@gmail.com>
2019-10-01 15:05:46 +0100

    Initial commit

---
+++ Multiply/src/com/ola/Miltiply.java
@@ -0,0 +1,11 @@
+package com.ola;
+
+/**
+ * The code does not execute properly. Try to figure out why.
+ */
+
+public class Miltiply {
+	public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
