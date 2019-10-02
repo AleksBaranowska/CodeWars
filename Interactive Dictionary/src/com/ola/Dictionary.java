ed73cbbeb4c7cf9df996f1152db5824783c08746
Aleksandra Baranowska <olaska@gmail.com>
2019-10-02 11:24:33 +0100

    Initial commit

---
+++ Interactive Dictionary/src/com/ola/Dictionary.java
@@ -0,0 +1,29 @@
+package com.ola;
+
+/**
+ * In this kata, your job is to create a class Dictionary which you can add
+ * words to and their entries. Example:
+ * >>> Dictionary d = new Dictionary();
+ * >>> d.newEntry("Apple", "A fruit that grows on trees");
+ * >>> System.out.println(d.look("Apple")); 
+ * A fruit that grows on trees * 
+ * >>> System.out.println(d.look("Banana")); 
+ * Cant find entry for Banana 
+ */
+
+{
+	String word;
+	String definition;
+	
+	public void newEntry(String a,String b){
+		this.word=a;
+		this.definition=b;
+		}
+	
+	public String look(String a){
+		if(a.equals(word))
+			return this.definition;
+		else 
+			return"Cant find entry for "+a;
+		}
+}
