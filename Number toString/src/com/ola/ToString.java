ea9f2e5022b6622975e2e1c8ffdd42aca6d07ad6
Aleksandra Baranowska <olaska@gmail.com>
2019-10-02 13:36:27 +0100

    Initial commit

---
+++ Number toString/src/com/ola/ToString.java
@@ -0,0 +1,10 @@
+package com.ola;
+
+/**
+ * The code gives an error!
+ * public static final String a = 123.toString();
+ */
+
+public class ToString {
+	  public static final String a = Integer.toString(123);
+	}
