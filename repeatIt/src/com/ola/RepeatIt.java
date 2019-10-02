4af0df1629a7a06234e899c098ec498901501d7a
Aleksandra Baranowska <olaska@gmail.com>
2019-10-02 13:40:51 +0100

    Initial commit

---
+++ repeatIt/src/com/ola/RepeatIt.java
@@ -0,0 +1,24 @@
+package com.ola;
+
+/**
+ * Create a function that takes a string and an integer (n).
+ * The function should return a string that repeats the input string n number of
+ * times.
+ * If anything other than a string is passed in you should return "Not a string"
+ * Example
+ * "Hi", 2 --> "HiHi" 
+ * 1234, 5 --> "Not a string"
+ */
+
+public class RepeatIt {
+	
+	public static String repeatString(final Object toRepeat, final int n) {
+		String repeated = "";
+		if (toRepeat instanceof String)
+			repeated = new String(new char[n]).replace("\0", (CharSequence) toRepeat);
+		else
+			repeated = "Not a string";
+		return repeated;
+	}
+
+}
