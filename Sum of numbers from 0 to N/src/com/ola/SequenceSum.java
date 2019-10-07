86a889b8c5b7260acea4c8130a033e0c8717d870
Aleksandra Baranowska <olaska@gmail.com>
2019-10-07 11:19:57 +0100

    Initial commit

---
+++ Sum of numbers from 0 to N/src/com/ola/SequenceSum.java
@@ -0,0 +1,47 @@
+package com.ola;
+
+/**
+ * Description:
+ * We want to generate a function that computes the series starting from 0 and
+ * ending until the given number following the sequence:
+ * 0 1 3 6 10 15 21 28 36 45 55 ....
+ * which is created by
+ * 0, 0+1, 0+1+2, 0+1+2+3, 0+1+2+3+4, 0+1+2+3+4+5, 0+1+2+3+4+5+6,
+ * 0+1+2+3+4+5+6+7 etc..
+ * Input:
+ * LastNumber
+ * Output:
+ * series and result
+ * Example: 
+ * Input: > 6 
+ * Output: 0+1+2+3+4+5+6 = 21
+ * Input: > -15 
+ * Output: -15<0
+ * Input: > 0 
+ * Output: 0=0
+ */
+
+public class SequenceSum {
+	
+	public static String showSequence(int value) {
+		int sum = 0;
+		StringBuilder sb = new StringBuilder();
+		if (value == 0) {
+			sb.append(value + "=0");
+		} else if (value < 0) {
+			sb.append(value + "<0");
+		} else
+			for (int i = 0; i <= value; i++) {
+				sum += i;
+				if (value == i) {
+					sb.append(value).append(" = ").append(sum);
+					System.out.println(sum);
+				}
+				else
+					sb.append(i).append("+");
+			}
+		// System.out.println(sb.toString());
+		return sb.toString();
+	}
+	
+}
