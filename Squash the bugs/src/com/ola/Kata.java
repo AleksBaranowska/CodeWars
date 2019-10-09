3dd3f16f76afe01846e4215f1bce8daefd7dbdd6
Aleksandra Baranowska <olaska@gmail.com>
2019-10-09 13:48:00 +0100

    Initial commit

---
+++ Squash the bugs/src/com/ola/Kata.java
@@ -0,0 +1,22 @@
+package com.ola;
+
+/**
+ * Simple challenge - eliminate all bugs from the supplied code so that the code
+ * runs and outputs the expected value. Output should be the length of the
+ * longest word, as a number.
+ * There will only be one 'longest' word.
+ */
+public class Kata {
+
+	public static int findLongest(final String str) {
+		String[] spl = str.split(" ");
+		int longest = 0;
+		for (int i = 0; i < spl.length; i++) {
+			if (spl[i].length() > longest)
+				longest = spl[i].length();
+		}
+		return longest;
+
+	}
+
+}
